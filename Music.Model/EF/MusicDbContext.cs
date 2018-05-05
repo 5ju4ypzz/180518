ade6fbac2b733a2b81508aa13efcd24c047b401b
VIP <VIP@ASP>
2018-05-05 16:12:24 -0700

    Thây đổi về DB

--- Music.Model/EF/MusicDbContext.cs
+++ Music.Model/EF/MusicDbContext.cs
@@ -1,6 +1,9 @@
 namespace Music.Model.EF
 {
+    using System;
     using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
 
     public partial class MusicDbContext : DbContext
     {
--- Music.Model/EF/user.cs
+++ Music.Model/EF/user.cs
@@ -4,6 +4,7 @@ namespace Music.Model.EF
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
 
     [Table("user")]
     public partial class user
