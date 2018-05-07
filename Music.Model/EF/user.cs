3f8ef6b7a3424d6d4aee6365de49e04e5ffad2a0
asp <asp@ASP>
2018-05-07 15:52:06 +0700

    Đăng ký thành công. Nhưng chưa kiễm tra trùng người dùng.

--- Music.Model/EF/user.cs
+++ Music.Model/EF/user.cs
@@ -1,4 +1,4 @@
-namespace Music.Model.EF
+﻿namespace Music.Model.EF
 {
     using System;
     using System.Collections.Generic;
@@ -21,25 +21,33 @@ namespace Music.Model.EF
         public int user_id { get; set; }
 
         [StringLength(100)]
+        [Display(Name = "Email")]
         public string email { get; set; }
 
         [StringLength(32)]
+        [Display(Name = "Mật khẩu")]
         public string password { get; set; }
 
         [StringLength(50)]
+        [Display(Name = "Họ tên")]
         public string user_name { get; set; }
 
+        [Display(Name = "Ngày sinh")]
         public DateTime? user_dateofbirth { get; set; }
 
+        [Display(Name = "Giới tính")]
         public bool? user_sex { get; set; }
 
         [StringLength(100)]
+        [Display(Name = "Địa chỉ")]
         public string user_address { get; set; }
 
         [Column(TypeName = "numeric")]
+        [Display(Name = "Số điện thoại")]
         public decimal? user_phone { get; set; }
 
         [StringLength(250)]
+        [Display(Name = "Hình đại diện")]
         public string user_image { get; set; }
 
         public int? user_level { get; set; }
